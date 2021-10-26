38c5f7a347703756bfda4e810b3a0ff805719faa
Sunny NG <ngsanluk@gmail.com>
2021-10-27 00:41:50 +0800

    first commit

---
+++ XamarinFormsLuckyNumber/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsLuckyNumber
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnPlay(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine("OnPlay() is called");
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            luckyNumber.Text = number.ToString();
+        }
+    }
+}
